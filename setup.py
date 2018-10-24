808f4c04d41558eaf264b3b366e73d760a5ba1ef
jbae11 <jbae11@illinois.edu>
2018-10-24 12:01:04 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pris_input_gen',
+      version='0.1',
+      description='Cyclus input generation from PRIS database',
+      url='http://github.com/arfc/pris-input-gen',
+      author='Jin Whan Bae',
+      author_email='jbae11@illinois.edu',
+      packages=['write_input'])
